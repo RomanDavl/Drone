0e81fe2291fff8984713f69c73e919f091ed5989
tereza1811 <tereza.kempkens@stud.uni-due.de>
2024-08-07 13:47:57 +0200

    Update WaypointContainer.cs
    Distance Fehler

--- Assets/Scripts/WaypointContainer.cs
+++ Assets/Scripts/WaypointContainer.cs
@@ -5,42 +5,92 @@ using UnityEngine;
 
 public class WaypointContainer : MonoBehaviour
 {
-    public List<Transform> waypoints;
+    public List<Transform> waypoints = new List<Transform>();
+    public List<Transform> waypointsToTarget = new List<Transform>();
     public float distanceTotal = 0;
-    private Transform prev=null;
+    public float distanceBisDrohnenshot = 0;
+    private Transform prev = null;
     private float distance;
 
     void Awake()
     {
+        // Initialize and add all child transforms except the parent itself
         foreach (Transform t in gameObject.GetComponentsInChildren<Transform>())
         {
-            
-
             waypoints.Add(t);
         }
-        waypoints.Remove(waypoints[0]);
-       
+        if (waypoints.Count > 0)
+        {
+            waypoints.RemoveAt(0); // Remove the parent transform
+        }
     }
 
-    public float GetDistanceTotal ()
+    public float GetDistanceTotal()
     {
-        foreach (Transform t in waypoints)
+        // Reset previous transform and total distance
+        prev = null;
+        distanceTotal = 0;
+
+        for (int i = 0; i < waypoints.Count; i++)
         {
             if (prev == null)
             {
-                prev = t;
-
+                prev = waypoints[i];
             }
             else
             {
-                distance = Vector3.Distance(prev.transform.position, t.transform.position);
-                //Debug.Log(distance);
+                distance = Vector3.Distance(prev.position, waypoints[i].position);
                 distanceTotal += distance;
-                prev = t;
+                prev = waypoints[i];
             }
         }
         return distanceTotal;
     }
 
-   
+    public float GetDistanceBisDrohnenshot(List<Transform> list)
+    {
+        // Reset previous transform and partial distance
+        prev = null;
+        distanceBisDrohnenshot = 0;
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (prev == null)
+            {
+                prev = list[i];
+            }
+            else
+            {
+                distance = Vector3.Distance(prev.position, list[i].position);
+                distanceBisDrohnenshot += distance;
+                prev = list[i];
+                Debug.Log("One distance: " + distance);
+            }
+        }
+
+        Debug.Log("Hallo");
+        Debug.Log("DistanceBisDrohnenshot: " + distanceBisDrohnenshot);
+        Debug.Log("Hallo2");
+
+        return distanceBisDrohnenshot;
+    }
+
+    public List<Transform> GetWaypointUpToIndex(int index)
+    {
+        List<Transform> list = new List<Transform>();
+        for (int i = 0; i <= index && i < waypoints.Count; i++)
+        {
+            list.Add(waypoints[i]);
+        }
+        return list;
+    }
+
+    private void Start()
+    {
+        List<Transform> list = GetWaypointUpToIndex(4);
+        GetDistanceBisDrohnenshot(list);
+    }
 }
+
+
+
